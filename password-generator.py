387647dadb34424487340efb86cfaa27a1b7bdca
Liduvo <69817471+Liduvo@users.noreply.github.com>
2021-11-18 22:22:39 +0300

    Add files via upload

---
+++ password-generator.py
@@ -0,0 +1,152 @@
+from random import choice
+
+print("-----------------------------------------------------------------------------------")
+print("                              Python Password Generator                              ")
+print(" ")
+print("      (1) Generate Random Password    (2)  Kullanıcı Girişili Şifre İçin           ")
+print("-----------------------------------------------------------------------------------")
+
+password_type_select = int(input())
+
+uppercase_and_small_letter = ["A","a","B","b","C","c","D","d","E","e","F","f","G","g","H","h","I","i","J","j","K","k","L","l","M","m","N","n","O","o","P","p","Q","q","R","r","S","s","T","t","U","u","V","v","W","w","X","x","Y","y","Z","z"]
+uppercase_letter = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+small_letter = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+special_character = ["+","×","÷","=","/","€","£","¥","₺",")","(","*","&","^","%","$","@","!","-",":",";","?","`","~","|","<",">","{","}","[","]","?","*"]
+numbers = [0,1,2,3,4,5,6,7,8,9]
+char = []
+password_list = []
+password = " "
+
+if password_type_select == 1:
+    print("You Have Chosen To Generate Random Password")
+    print("How long is your password?")
+
+    password_length = int(input())
+
+    print("Would you like to use numbers?")
+    print("(1) Yes        (2) No  ")
+
+    password_number_random = int(input())
+
+    print("Do you want special characters in your password?")
+    print("(1) Yes        (2) No  ")
+
+    password_special_characters_random = int(input())
+
+    print("Do you want upper/lowercase letters in your password?")
+    print("(1) Only Uppercase Letter  (2) Only Small Letter (3) Uppercase Letter and Small Letter")
+
+    password_letter_random = int(input())
+
+
+    if  password_number_random == 1 and password_special_characters_random == 1 and password_letter_random == 1:
+
+         char = numbers + special_character + uppercase_letter
+         for i in range(password_length):
+             password += str(choice(char))
+
+
+    if password_number_random == 1 and password_special_characters_random == 1 and password_letter_random == 2:
+
+        char = numbers + special_character + small_letter
+        for i in range(password_length):
+            password += str(choice(char))
+
+
+    if password_number_random == 1 and password_special_characters_random == 1 and password_letter_random == 3:
+        char = numbers + special_character + uppercase_and_small_letter
+        for i in range(password_length):
+            password += str(choice((char)))
+
+
+    if password_number_random == 1 and password_special_characters_random == 2 and password_letter_random == 1:
+        char = numbers + uppercase_letter
+        for i in range(password_length):
+            password += str(choice(char))
+
+
+    if password_number_random == 1 and password_special_characters_random == 2 and password_letter_random == 2:
+        char = numbers + small_letter
+        for i in range(password_length):
+            password += str(choice(char))
+
+
+    if password_number_random == 1 and password_special_characters_random == 2 and password_letter_random == 3:
+        char = numbers + uppercase_and_small_letter
+        for i in range(password_length):
+            password += str(choice(char))
+
+
+    if password_number_random == 2 and password_special_characters_random == 1 and password_letter_random == 1:
+        char = special_character + uppercase_letter
+        for i in range(password_length):
+            password += str(choice(char))
+
+
+    if password_number_random == 2 and password_special_characters_random == 1 and password_letter_random == 2:
+        char = special_character + small_letter
+        for i in range(password_length):
+            password += str(choice(char))
+
+
+    if password_number_random == 2 and password_special_characters_random == 1 and password_letter_random == 3:
+        char = special_character + uppercase_and_small_letter
+        for i in range(password_length):
+            password += str(choice(char))
+
+
+    if password_number_random == 2 and password_special_characters_random == 2 and password_letter_random == 1:
+
+         for i in range(password_length):
+             password += str(choice(uppercase_letter))
+
+
+    if password_number_random == 2 and password_special_characters_random == 2 and password_letter_random == 2:
+
+        for i in range(password_length):
+            password += str(choice(small_letter))
+
+
+    if password_number_random == 2 and password_special_characters_random == 2 and password_letter_random == 3:
+
+        for i in range(password_length):
+            password += str(choice(uppercase_and_small_letter))
+
+    else:
+        print("Try again.")
+
+    print(password)
+
+if password_type_select == 2:
+   print("You have chosen the User Login Password.")
+   print("How long should the password be?")
+
+   password_length = int(input())
+
+   print("What letters would you like to use in the password?")
+
+   password_request = str(input())
+
+   print("Do you want to use numbers and special characters?")
+   print("(1) Yes   (2) No")
+
+   password_number_special_characters = int(input())
+   if password_number_special_characters == 1:
+
+       print("What numbers and special phrases would you like to have in the password?")
+
+       password_number = int(input())
+
+
+   for i in password_request:
+       password_list += i
+
+
+   for i in str(password_number):
+       password_list += i
+
+   for i in range(password_length):
+       password += str(choice(password_list))
+
+
+   print(password)
